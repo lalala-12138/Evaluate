524f7f0b07993699202ba653266ad131c1fe9e6d
lalala-12138 <101035003+lalala-12138@users.noreply.github.com>
2022-07-16 00:17:13 +0800

    Update demo.user.js

--- demo.user.js
+++ demo.user.js
@@ -10,38 +10,38 @@
 // @icon         https://www.google.com/s2/favicons?domain=xatu.edu.cn
 // @grant        none
 // ==/UserScript==
-function sleep(time, unit){
-    if(time == null){time = 5000;}
-    if(unit != null){time = time * 1000;}
-    for(var t = Date.now();Date.now() - t <= time;);
+function sleep(time, unit) {
+    if (time == null) { time = 5000; }
+    if (unit != null) { time = time * 1000; }
+    for (var t = Date.now(); Date.now() - t <= time;);
 }
-function check(){
-        var box=document.getElementsByClassName("qBox objective required");
-        console.log(box.length);
-        for(var n=1;n<=box.length;n++){
-            var m=40+n;
-            var b=document.querySelector("#question_"+m+" li:last-child input");
-            //var b=box[9];
-            b.setAttribute("checked", "checked");
+function check() {
+    var box = document.getElementsByClassName("qBox objective required");
+    console.log(box.length);
+    for (var n = 1; n <= box.length; n++) {
+        var m = 40 + n;
+        var b = document.querySelector("#question_" + m + " li:last-child input");
+        //var b=box[9];
+        b.setAttribute("checked", "checked");
 
-        }
-        document.querySelector("textarea").value="非常好";
-        setTimeout(() => document.querySelector("#sub").click(), 500);
+    }
+    document.querySelector("textarea").value = "非常好";
+    setTimeout(() => document.querySelector("#sub").click(), 500);
 }
-function a(){
+function a() {
     console.log('confirm')
     return true;
-  }
+}
 window.confirm = a;
 
-if(location.href.indexOf("innerIndex.action")!= -1 | location.href.indexOf("stdEvaluate.action")!= -1){
+if (location.href.indexOf("innerIndex.action") != -1 | location.href.indexOf("stdEvaluate.action") != -1) {
     console.log(location.href);
     sleep(1000);
     document.getElementsByClassName("eval")[0].click();
 }
 
-else if(location.href.indexOf("answer.action")!= -1){
-    window.onload=function(){
+else if (location.href.indexOf("answer.action") != -1) {
+    window.onload = function () {
         sleep(400);
         setTimeout(() => check(), 1000);
     }
